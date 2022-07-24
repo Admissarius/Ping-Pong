2a895ce28f9f03e070f8d6f16d2319d624ddad50
Maxsim Eduardovich <maxsimscherbakoww@mail.ru>
2022-07-24 18:08:27 +0300

    player_is_done

---
+++ scripts/game.js
@@ -0,0 +1,54 @@
+import { Canvas } from "./canvas.js";
+import { Controll } from "./control.js";
+import { Player } from "./player.js";
+import { RenderUI } from "./renderUI.js";
+
+class Game {
+    constructor() {
+        this.screen = new Map([
+            ['background', new Canvas()],
+            ['gamelayer', new Canvas()],
+            ['ui', new Canvas()]
+        ])
+
+        this.control_1 = new Controll([ [38, 'up'], [40, 'down'] ])
+        this.control_2 = new Controll([ [87, 'up'], [83, 'down'] ])
+
+        this.player1 = new Player(this, 10, 200, this.control_1, '#52C5D4')
+        this.player2 = new Player(this, this.screen.get('gamelayer').element.width - 20, 200, this.control_2, '#EDEDED')
+
+        this.UI = new RenderUI(this)
+
+        this.init()
+    }
+
+    init() {
+
+        //background
+        this.screen.get('background').fill('#424357')
+        this.screen.get('background').drawRect((this.screen.get('background').element.width / 2) - 3, 0, 6, this.screen.get('background').element.height, '#585874')
+        this.screen.get('background').drawCircle((this.screen.get('background').element.width / 2), (this.screen.get('background').element.height / 2), 100, 6, '#585874', false)
+
+        requestAnimationFrame( time => this.loop(time) )
+    }
+
+    update(time) {
+        this.player1.update(time)
+        this.player2.update(time)
+    }
+
+    loop(time) {
+        this.screen.get('gamelayer').clear()
+
+        this.update(time)
+
+        this.player1.draw()
+        this.player2.draw()
+
+        requestAnimationFrame( time => this.loop(time) )
+    }
+}
+
+window.onload = () => {
+    const game = new Game()
+}
